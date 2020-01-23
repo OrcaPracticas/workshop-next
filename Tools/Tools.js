ffeb975af3d9f9732bbb73cb811b7975aae372de
Jmendez <jorge.mendez.ortega@gmail.com>
2020-01-22 21:07:40 -0600

    ➕ADD: Tools
    Filtrado de datos para construcción del listado de paginas 
    
    Issue #14
    
    -

---
+++ Tools/Tools.js
@@ -0,0 +1,46 @@
+class Tools {
+    static getDataCard(data, link = "/channel") {
+        const DATA = data.map((item) => {
+            const { id, title, urls = {} } = item;
+            const { logo_image: { original = "" } } = urls;
+            return {
+                id,
+                logo: original,
+                title,
+                uri: `${link}?id=${id}`,
+            };
+        });
+        return DATA;
+    }
+
+    static getList(data, link = "/podcast") {
+        const DATA = data.map((item) => {
+            const {
+                id, duration,
+                title,
+            } = item;
+            return {
+                id,
+                duration,
+                title,
+                uri: `${link}?id=${id}`,
+            };
+        });
+        return DATA;
+    }
+
+    static getHeader(data, type = "") {
+        const {
+            title,
+            urls: { logo_image: { original: logo } },
+        } = data;
+        return {
+            logo,
+            title,
+            type,
+            subTitle: title,
+        };
+    }
+}
+
+export default Tools;
