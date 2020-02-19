9731bb1c41439912e3c2408cc5f2eb6339fd980b
Jorge Mendez Ortega <jorge.mendez.ortega@gmail.com>
2020-02-18 20:59:54 -0600

    🛠 FIX: Correción json
    Se corrigen los props enviados por el mockup de prueba
    
    Issue #23

--- router.js
+++ router.js
@@ -3,4 +3,5 @@ const routes = module.exports = require("next-routes")()
 routes
     .add("home", "/", "index")
     .add("channel", "/:slug.:id", "channel")
-    .add("podcast", "/:slugChannel.:idChannel/:slug.:id", "podcast");
+    .add("podcast", "/:slugChannel.:idChannel/:slug.:id", "podcast")
+    .add("test", "/test", "test");
